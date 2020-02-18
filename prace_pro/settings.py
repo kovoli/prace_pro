51797578a2c363cf5e1d7985216afe42910e69a2
Oleg <kovoli1985@gmail.com>
2020-02-18 19:25:16 +0100

    Update settings.py

--- prace_pro/settings.py
+++ prace_pro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k6izty@2!e))f*+w+xfc6vgje=&b88ofh5t)$g$!49xw_ba_it'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
