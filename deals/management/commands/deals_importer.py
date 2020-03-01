b176a3d8e5243b4c43a27c8333ae6fb5cad63936
imac <kovoli1985@gmail.com>
2020-03-01 20:27:23 +0100

    new

---
+++ deals/management/commands/deals_importer.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from deals.models import Deal, Brand, Category, Shop
+
+
+from django.core.files.base import ContentFile
+from io import BytesIO
+from urllib.request import urlopen
+
+
+# ----- Main Command -----
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('file_name', type=str, help='Название файла')
+
+    def handle(self, *args, **kwargs):
+        data = {'name': 'deal_name',
+                'description': 'description',
+                'author': 'user',
+                'price': 4654,
+                'oldprice': 255,
+                'link_to_shop': 'url()',
+                'category': 'category',
+                'brand': 'brand',
+                'shop': 'shop'
+                }
